d10bf90eb3880a0714852a0d64e3b5ca77cc63f4
bransynluther <bransynluther@gmail.com>
2017-10-23 10:01:08 -0400

    Additional view rework for input controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170929170642) do
+ActiveRecord::Schema.define(version: 20171017150340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20170929170642) do
   end
 
   create_table "plain_text_inputs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_inputs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
